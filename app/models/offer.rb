6f8dd860fd3afe3e0e01640ed98c4d7fb413e0aa
Cyril Chehovah <c.chehovah@gmail.com>
2015-11-02 18:49:36 +0100

    create model Offer

---
+++ app/models/offer.rb
@@ -0,0 +1,4 @@
+class Offer < ActiveRecord::Base
+  belongs_to :book
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102173107) do
+ActiveRecord::Schema.define(version: 20151102174819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20151102173107) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "offers", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "user_id"
+    t.boolean  "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "offers", ["book_id"], name: "index_offers_on_book_id", using: :btree
+  add_index "offers", ["user_id"], name: "index_offers_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -46,4 +57,6 @@ ActiveRecord::Schema.define(version: 20151102173107) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "offers", "books"
+  add_foreign_key "offers", "users"
 end
