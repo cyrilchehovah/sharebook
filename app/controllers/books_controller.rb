9df39d76094fa74cacc54bb44e0735b389cc7b70
Lisa Durand <lisa@superdudu.net>
2015-11-03 16:43:58 +0100

    Index books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,17 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+
+    @array = []
+    @books.each do |book|
+      if book.offers[0] != nil
+        @array << book
+      end
+    end
+
+  end
+
+  # si un book a au moins 1 offer, il s'affiche
+  #sinon il ne s'affiche pas
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :offers, only: [:index, :new, :create]
 
+  resources :books, only: [:index]
+
   root to: "pages#home"
 
   # The priority is based upon order of creation: first created -> highest priority.
