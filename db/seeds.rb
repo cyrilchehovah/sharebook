6b26040f30aa18bfb076753fb2d0e36b2acdf080
Lisa Durand <lisa@superdudu.net>
2015-11-03 13:38:06 +0100

    Seed of 5 books created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Book.destroy_all
+
+books = [
+
+  {
+    title: 'Les fondamentaux de la typographie',
+    description: 'Cette version revue et remaquettée des fondamentaux de la typographie explore les principes théoriques de la typographie et leurs multiples applications. Il passe en revue les différents paramètres d une composition typographique et se penche sur l utilisation créative des caractères, tant sur les supports imprimés qu à l écran. Les principes fondamentaux de la typographie sont expliqués pas à pas et accompagnés d exemples de compositions anciennes ou contemporaines.',
+    category: 'Typographie',
+    author: 'Gavin Ambrose'
+  },
+
+  {
+    title: 'Les fondamentaux du graphisme',
+    description: "Graphisme offre une vue d'ensemble unique sur les multiples facettes de l'industrie du graphisme et ses procédés. Divisé en six chapitres, l'ouvrage aborde le graphisme en tant que discipline et se penche sur ses influences et éléments de création, sur les processus graphique et de production, ainsi que sur la meilleure manière de transmettre des messages efficaces et de s'autopromouvoir.",
+    category: 'Graphisme',
+    author: 'Paul Harris'
+  },
+
+  {
+    title: 'Concevez votre site web avec PHP et MySQL',
+    description: "Blogs, réseaux sociaux, pages d'accueil personnalisables... Depuis quelques années, les sites web ont gagné en fonctionnalités et sont devenus dans le même temps de plus en plus complexes. Que le temps de la 'page web perso' est loin ! Aujourd'hui, il faut que ça bouge !",
+    category: 'Developpement',
+    author: 'Mathieu Nebra '
+  },
+
+  {
+    title: 'Design émotionnel',
+    description: 'Engagez émotionnellement les visiteurs de votre site Web en leur offrant une expérience positive grâce aux préceptes énoncés dans ce petit et brillant ouvrage écrit par le designer spécialiste en expérience utilisateur, Aarron Walter.',
+    category: 'UX',
+    author: 'Aarron Walter'
+  },
+
+  {
+    title: 'Mobile First',
+    description: "Le designer de renom, ancien architecte du design de Yahoo!, Luke Wroblewski, nous fait partager dans ce petit guide stratégique toutes ses connaissances et son savoir-faire en matière d'expérience mobile.",
+    category: 'Webdesign',
+    author: 'Luke Wroblewski'
+  }
+]
+
+books.each {|book| Book.create(book)}
