b5abda8fd57fd4fbdc935ff8f319028723d13394
Lisa Durand <lisa@superdudu.net>
2015-11-03 14:32:24 +0100

    work on offers controller and created routes new and create for offer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,16 +1,34 @@
 class OffersController < ApplicationController
-  before_action :set_book
+  before_action :set_book, only: [:create]
+  before_action :set_user, only: [:create]
 
   def new
-    @offer = @book.offers.build
+    @offer = Offer.new
   end
 
   def create
+    @offer = Offer.new(params_offer)
+    @offer.book = @book
+    @offer.user = @user
+
+      if @offer.valid?
+        @offer.save
+        # redirect_to cocktail_path(@cocktail)
+      else
+        render :new
+      end
   end
 
   private
   def set_book
     @book = Book.find(params[:book_id])
+  end
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def params_offer
+    params.require(:offer).permit(:user_id, :book_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  resources :offers, only: [:new, :create]
+
   root to: "pages#home"
 
   # The priority is based upon order of creation: first created -> highest priority.
