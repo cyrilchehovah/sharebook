97a0de367f7461506b7054bae7c2222aa365821c
Lisa Durand <lisa@superdudu.net>
2015-11-03 13:53:45 +0100

    Dependencies models + begining of controller Offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,16 @@
 class OffersController < ApplicationController
+  before_action :set_book
+
+  def new
+    @offer = @book.offers.build
+  end
+
+  def create
+  end
+
+  private
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
 end
