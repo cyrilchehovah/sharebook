197db145a9a2642ef1e32ca9da2ab37a34b04f20
Lisa Durand <lisa@superdudu.net>
2015-11-04 13:55:09 +0100

    creation de Users controller + route show + view show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  resources :users, only: [:show]
+
   resources :offers, only: [:index, :new, :create]
 
   resources :books, only: [:index]
