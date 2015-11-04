da9e27b679b3f423c271143597dfa6ebca63b54b
Camille Delbove <camille.delbove@gmail.com>
2015-11-04 12:16:25 +0100

    Added seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ books = [
     description: 'Cette version revue et remaquettée des fondamentaux de la typographie explore les principes théoriques de la typographie et leurs multiples applications. Il passe en revue les différents paramètres d une composition typographique et se penche sur l utilisation créative des caractères, tant sur les supports imprimés qu à l écran. Les principes fondamentaux de la typographie sont expliqués pas à pas et accompagnés d exemples de compositions anciennes ou contemporaines.',
     category: 'Typographie',
     author: 'Gavin Ambrose',
-    # picture: 'http://ecx.images-amazon.com/images/I/31fxBpqHnRL._SX322_BO1,204,203,200_.jpg'
+    image: 'http://ecx.images-amazon.com/images/I/41v5YywoYyL._SX439_BO1,204,203,200_.jpg'
   },
 
   {
@@ -24,7 +24,7 @@ books = [
     description: "Graphisme offre une vue d'ensemble unique sur les multiples facettes de l'industrie du graphisme et ses procédés. Divisé en six chapitres, l'ouvrage aborde le graphisme en tant que discipline et se penche sur ses influences et éléments de création, sur les processus graphique et de production, ainsi que sur la meilleure manière de transmettre des messages efficaces et de s'autopromouvoir.",
     category: 'Graphisme',
     author: 'Paul Harris',
-    # picture: 'http://ecx.images-amazon.com/images/I/31fxBpqHnRL._SX322_BO1,204,203,200_.jpg'
+    image: 'http://ecx.images-amazon.com/images/I/51Tb-MJIsGL._SX426_BO1,204,203,200_.jpg'
 
   },
 
@@ -33,7 +33,7 @@ books = [
     description: "Blogs, réseaux sociaux, pages d'accueil personnalisables... Depuis quelques années, les sites web ont gagné en fonctionnalités et sont devenus dans le même temps de plus en plus complexes. Que le temps de la 'page web perso' est loin ! Aujourd'hui, il faut que ça bouge !",
     category: 'Developpement',
     author: 'Mathieu Nebra',
-    # picture: 'http://ecx.images-amazon.com/images/I/31fxBpqHnRL._SX322_BO1,204,203,200_.jpg'
+    image: 'http://ecx.images-amazon.com/images/I/41Q5qRj-f1L._SX346_BO1,204,203,200_.jpg'
 
   },
 
@@ -42,7 +42,7 @@ books = [
     description: 'Engagez émotionnellement les visiteurs de votre site Web en leur offrant une expérience positive grâce aux préceptes énoncés dans ce petit et brillant ouvrage écrit par le designer spécialiste en expérience utilisateur, Aarron Walter.',
     category: 'UX',
     author: 'Aarron Walter',
-    picture: 'http://ecx.images-amazon.com/images/I/31fxBpqHnRL._SX322_BO1,204,203,200_.jpg'
+    image: 'http://ecx.images-amazon.com/images/I/41kftZ9jqCL._SX328_BO1,204,203,200_.jpg'
 
   },
 
@@ -51,7 +51,7 @@ books = [
     description: "Le designer de renom, ancien architecte du design de Yahoo!, Luke Wroblewski, nous fait partager dans ce petit guide stratégique toutes ses connaissances et son savoir-faire en matière d'expérience mobile.",
     category: 'Webdesign',
     author: 'Luke Wroblewski',
-    # picture: 'http://ecx.images-amazon.com/images/I/31fxBpqHnRL._SX322_BO1,204,203,200_.jpg'
+    image: 'http://ecx.images-amazon.com/images/I/31fxBpqHnRL._SX322_BO1,204,203,200_.jpg'
 
   }
 ]
