9321b8ba7cd33d6595ab36bb47d7e5f2be04609d
Cyril Chehovah <c.chehovah@gmail.com>
2015-11-04 14:43:40 +0100

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,8 @@ ActiveRecord::Schema.define(version: 20151104104647) do
     t.text     "description"
     t.string   "category"
     t.string   "author"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.string   "picture_file_name"
-    t.string   "picture_content_type"
-    t.integer  "picture_file_size"
-    t.datetime "picture_updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image"
   end
 
