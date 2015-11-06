f5de262fc75fd62c3e98714ed80b5c1abf205a21
Camille Delbove <camille.delbove@gmail.com>
2015-11-06 10:54:51 +0100

    booking ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,27 +1,9 @@
 class BookingsController < ApplicationController
-  before_action :set_book, only: [:create, :show]
-  before_action :set_user, only: [:create, :new]
 
   def create
     @offer = Offer.find(params[:offer_id])
     @offer.bookings.create(user_id: current_user.id)
     redirect_to user_path(current_user)
-  end
-
-  def show
-    @booking = Booking.find(params[:id])
-  end
-
-
-  private
-  def set_book
-    @book = Book.find(params[:booking][:offer_id, :book_id])
-  end
-  def set_offer
-    @user = current_user
-  end
-  def params_booking
-    params.require(:booking).permit(:user_id, :offer_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @offers = @user.offers
+    @bookings = @user.bookings
 
 
     @books = []
