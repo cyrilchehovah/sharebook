f2ad83b15ea301d5f44ac5ec375dacb76c812ad7
Camille Delbove <camille.delbove@gmail.com>
2015-11-09 10:44:08 +0100

    fake branche

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
 
   def create
+    raise
     @offer = Offer.find(params[:offer_id])
     @offer.bookings.create(user_id: current_user.id)
     redirect_to user_path(current_user)
