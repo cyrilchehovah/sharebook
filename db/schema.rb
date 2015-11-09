c04f111b17102963014867e03ac7ab814ce5c380
Camille Delbove <camille.delbove@gmail.com>
2015-11-09 15:06:59 +0100

    add input first last name and gem paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,12 @@ ActiveRecord::Schema.define(version: 20151109101601) do
     t.text     "description"
     t.string   "category"
     t.string   "author"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
     t.string   "image"
   end
 
