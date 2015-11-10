645bc50d7789d0f7356e8e9536d41a3572009bcf
Lisa Durand <lisa@superdudu.net>
2015-11-10 17:58:22 +0100

    Premier message créé lors d'une reservation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,6 +5,15 @@ class Booking < ActiveRecord::Base
   has_many :messages
 
   validates_inclusion_of :accepted, :in => [true, false]
+
+  after_create :send_first_message
+
+  private
+
+  def send_first_message
+    Message.create(content: "Salut, untel veut t'emprunter ce livre", user: user, booking: self)
+  end
+
 end
 
 
