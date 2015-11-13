f89b4671bfdb183ecd5ffa57abed81e0cc3ee27b
Camille Delbove <camille.delbove@gmail.com>
2015-11-13 12:02:58 +0100

    change redirect sign-in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,7 +15,11 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def after_update_path_for(resource)
-    user_path(resource)
+    user.show_path(resource)
+  end
+
+  def after_sign_up_path_for(resource)
+    user.edit_path(resource)
   end
 
 end
