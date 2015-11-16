520ec2353f65d14b9b9cfaeaab063489ad8b9ff8
Camille Delbove <camille.delbove@gmail.com>
2015-11-16 17:21:27 +0100

    ajout schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113143631) do
-
+ActiveRecord::Schema.define(version: 20151116151025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,13 +39,12 @@ ActiveRecord::Schema.define(version: 20151113143631) do
     t.text     "description"
     t.string   "category"
     t.string   "author"
-<<<<<<< HEAD
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-=======
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
->>>>>>> master
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
     t.string   "image"
     t.string   "publisher"
     t.integer  "pages_number"
@@ -60,17 +58,7 @@ ActiveRecord::Schema.define(version: 20151113143631) do
     t.integer  "booking_id"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-<<<<<<< HEAD
-=======
-=======
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-<<<<<<< HEAD
-=======
     t.string   "author"
->>>>>>> master
->>>>>>> master
->>>>>>> master
     t.datetime "read_at"
     t.boolean  "auto_generated", default: false
   end
